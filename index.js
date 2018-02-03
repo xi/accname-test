9d9f79560a47a74813b5e3e38a99df6571097b8e
Tobias Bengfort <tobias.bengfort@posteo.de>
2018-02-03 11:31:16 +0100

    fix file imports: specify encoding

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 var fs = require('fs');
 
 var files = [
-    fs.readFileSync(__dirname + '/tests/accdc/w3c-alternative-text-computation/focusable role=link.html'),
-    fs.readFileSync(__dirname + '/tests/accdc/w3c-alternative-text-computation/form field with aria-describedby + hidden.html'),
-    fs.readFileSync(__dirname + '/tests/accdc/w3c-alternative-text-computation/form field with aria-describedby.html'),
-    fs.readFileSync(__dirname + '/tests/accdc/w3c-alternative-text-computation/form field with aria-labelledby.html'),
-    fs.readFileSync(__dirname + '/tests/accdc/w3c-alternative-text-computation/form field with label element.html'),
-    fs.readFileSync(__dirname + '/tests/mozilla.html'),
-    fs.readFileSync(__dirname + '/tests/pseudo-element.html'),
-    fs.readFileSync(__dirname + '/tests/spec/example1.html'),
-    fs.readFileSync(__dirname + '/tests/spec/example2.html'),
-    fs.readFileSync(__dirname + '/tests/spec/example3.html'),
-    fs.readFileSync(__dirname + '/tests/spec/step2Fiii-ednote.html'),
+    fs.readFileSync(__dirname + '/tests/accdc/w3c-alternative-text-computation/focusable role=link.html', 'utf8'),
+    fs.readFileSync(__dirname + '/tests/accdc/w3c-alternative-text-computation/form field with aria-describedby + hidden.html', 'utf8'),
+    fs.readFileSync(__dirname + '/tests/accdc/w3c-alternative-text-computation/form field with aria-describedby.html', 'utf8'),
+    fs.readFileSync(__dirname + '/tests/accdc/w3c-alternative-text-computation/form field with aria-labelledby.html', 'utf8'),
+    fs.readFileSync(__dirname + '/tests/accdc/w3c-alternative-text-computation/form field with label element.html', 'utf8'),
+    fs.readFileSync(__dirname + '/tests/mozilla.html', 'utf8'),
+    fs.readFileSync(__dirname + '/tests/pseudo-element.html', 'utf8'),
+    fs.readFileSync(__dirname + '/tests/spec/example1.html', 'utf8'),
+    fs.readFileSync(__dirname + '/tests/spec/example2.html', 'utf8'),
+    fs.readFileSync(__dirname + '/tests/spec/example3.html', 'utf8'),
+    fs.readFileSync(__dirname + '/tests/spec/step2Fiii-ednote.html', 'utf8'),
 ];
 
 var getTests = function(root) {
