5bd2ac8479d46a271ae6e82f415be4bff8eae459
Tobias Bengfort <tobias.bengfort@posteo.de>
2019-04-20 15:27:01 +0200

    rm bundle
    leave bundeling to calling code

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-var fs = require('fs');
-
-var files = {
-    'labels': fs.readFileSync(__dirname + '/tests/labels.html', 'utf8'),
-    'owns': fs.readFileSync(__dirname + '/tests/owns.html', 'utf8'),
-    'mozilla': fs.readFileSync(__dirname + '/tests/mozilla.html', 'utf8'),
-    'pseudo-element': fs.readFileSync(__dirname + '/tests/pseudo-element.html', 'utf8'),
-    'spec example1': fs.readFileSync(__dirname + '/tests/spec/example1.html', 'utf8'),
-    'spec example2': fs.readFileSync(__dirname + '/tests/spec/example2.html', 'utf8'),
-    'spec example3': fs.readFileSync(__dirname + '/tests/spec/example3.html', 'utf8'),
-    'spec step2Fiii-ednote': fs.readFileSync(__dirname + '/tests/spec/step2Fiii-ednote.html', 'utf8'),
-    'w3c wiki': fs.readFileSync(__dirname + '/tests/w3c.html', 'utf8'),
-};
-
 var getTests = function(root) {
     var elements = root.querySelectorAll('.accname-test');
     return Array.prototype.map.call(elements, function(element) {
@@ -26,6 +12,5 @@ var getTests = function(root) {
 };
 
 module.exports = {
-    files: files,
     getTests: getTests,
 };
